4423e85bd45716677738d4524434143b60a7423e
Liviu975 <lio.dumitrescu@gmail.com>
2022-04-01 16:05:46 +0300

    create transaction method

--- dev/test.js
+++ dev/test.js
@@ -4,10 +4,9 @@ const Blockchain = require("./blockchain");
 const bitcoin = new Blockchain();
 
 bitcoin.createNewBlock(32893, 'O3392UI32', '03984IIEBUEG');
+// bitcoin.createNewBlock(32342, 'dino2UI32', '23984IIEBUEG');
+// bitcoin.createNewBlock(23425, 'OEIWE2UI3', '33984IIEBUEG');
+// bitcoin.createNewBlock(24234, 'Oewofu2UI', 'heu84IIEBUEG');
 
-bitcoin.createNewBlock(32342, 'dino2UI32', '23984IIEBUEG');
-bitcoin.createNewBlock(23425, 'OEIWE2UI32', '33984IIEBUEG');
-bitcoin.createNewBlock(24234, 'Oewofu2UI32', 'oieheu84IIEBUEG');
-
-
-console.log(bitcoin);
+bitcoin.createNewTransaction(100, "LIO039484", 'ANA039IRJ')
+console.log(bitcoin)
