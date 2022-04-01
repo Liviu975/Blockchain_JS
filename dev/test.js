5cba2aedb77f40432fb6cf74b8ee3c4d40d8378b
Liviu975 <lio.dumitrescu@gmail.com>
2022-04-01 16:19:06 +0300

    mine some blocks

--- dev/test.js
+++ dev/test.js
@@ -8,4 +8,12 @@ bitcoin.createNewBlock(32893, 'O3392UI32', '03984IIEBUEG');
 bitcoin.createNewTransaction(100, "LIO039484", 'ANA039IRJ');
 
 bitcoin.createNewBlock(1412, '09IOEB038IEN', '038EDBCUEBE');
-console.log(bitcoin)
+
+
+bitcoin.createNewTransaction(85, 'AURAS303974', 'LIO03297h');
+bitcoin.createNewTransaction(65, 'ANA993jdu7e', 'SABI08342');
+bitcoin.createNewTransaction(65, 'LIO9jedju7e', 'SABI0eu82');
+
+bitcoin.createNewBlock(8376, '08346829', '09474LIDUI');
+console.log(bitcoin.chain[2].transactions);
+console.log(bitcoin);
